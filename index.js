3e17f4d2847e305e7ccfb27c974cfeb2d253b3a6
mehedi-web2023 <courseweb38@gmail.com>
2023-08-17 19:39:17 +0600

    Array

---
+++ index.js
@@ -0,0 +1,61 @@
+// hello
+
+var name = "mehedi";
+
+var friend = [15, 16, 17]
+
+var friend = ["mehedi","sanjida","adiba"];
+
+array,index, length,indexof
+
+friend[1]="nus-00"
+friend[2]="sanjida"
+
+console.log(friend.indexOf("sanjida"))
+
+console.log(friend.length)
+
+
+
+console.log(5<6)
+console.log(5<=5)
+
+
+var name1= "Sanjida"
+console.log(name1 === "sanjida" )
+
+// && ||
+
+var jobkoro = false
+var income = 60000;
+var car = 0;
+
+if(jobkoro == true  && income > 50000 || car >= 1 ){
+    console.log("tumi biya korte parba")
+
+}
+else{
+        console.log("harami tor kopale biya nai")
+    }
+
+if(jobkoro == true  || income > 50000){
+    console.log("tumi biya korte parba")
+
+}
+else{
+    console.log('tor kopale biya nai')
+}
+// -----------------------------------------------------
+if(income > 60000){
+    console.log("tumi parba kiso korte")
+
+}
+else if(jobkoro == true){
+    console.log("tumi parba kiso korte")
+
+}
+
+else{
+    console.log("harami tor kopale biya nai")
+}
+
